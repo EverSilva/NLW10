ff0d98c977c7fc674054c6091d1af82625598785
eversilva <everson.silva2706@gmail.com>
2022-11-02 18:27:21 -0300

    Initial commit

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,7 @@
+import { Center, Text } from "native-base";
+
+export const SignIn = () => (
+    <Center flex={1} bgColor='gray.900'>
+        <Text color="violet.100" fontSize={24}>SignIn</Text>
+    </Center>
+)
